8da5ec7769ac66efe0ebee2c95f3367ddd67dcfd
Jermaine Forbes <jermaine0forbes@gmail.com>
2026-08-12 20:23:26 -0400

    generating fake data pt3

--- app2/app/lib/faker.ts
+++ app2/app/lib/faker.ts
@@ -1,20 +1,20 @@
 import {faker} from "@faker-js/faker";
-import type { UserCreateInput, RoleCreateInput, QuoteCreateInput, PlanCreateInput } from "../prisma/models";
+import type { UserCreateInput, RoleCreateInput, QuoteCreateWithoutUserInput, PlanCreateInput } from "../prisma/models";
 
 const { internet, helpers, lorem, date, number } = faker;
 
-export function createUser()
+export function createUser(index: number): UserCreateInput
 {
     let count: number = faker.number.int(20);
     const i : number = faker.number.int(index*100)
-    const quotes : QuoteCreateInput[] = createMultiQuotes(count);
-    const name = internet.username();
-    const roleId = helpers.arrayElement([1,2,3]);
     const time = date.recent();
+    const quotes : QuoteCreateWithoutUserInput[] = createMultiQuotes(count, time);
+    const name = internet.username();
+    const role = helpers.arrayElement(['USER', 'MODERATOR', 'ADMIN', 'SUPER_ADMIN']);
     return {
         name: name+i,
         email: name+i+"@gmail.com",
-        roleId: roleId,
+        role: role,
         createdAt: time,
         quotes: {
             create:quotes
@@ -22,19 +22,32 @@ export function createUser()
     }
 }
 
-export function createMultiUsers()
+export function createMultiUsers(count: number): UserCreateInput[]
 {
-
+    const users :  UserCreateInput[] = [];
+        while( count > 0) {
+        users.push(createUser(count))
+        count--;
+    }
+    return users;
 }
 
-export function createQuote()
+export function createQuote(time: Date): QuoteCreateWithoutUserInput
 {
-
+    return {
+        message: lorem.text(),
+        createdAt: time
+    };
 }
 
 
-export function createMultiQuotes(count: number)
+export function createMultiQuotes(count: number, time: Date)
 {
-
+        const quotes :  QuoteCreateWithoutUserInput[] = [];
+        while( count > 0) {
+        quotes.push(createQuote(time))
+        count--;
+    }
+    return quotes;
 }
 
